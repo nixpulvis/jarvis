84a78966185fac3e49bd0f12f8f109fa9b081c44
Epicgrim <nathanlil13@me.com>
2012-10-30 23:21:53 -0400

    small change from backticks to kernal#system

--- lib/jarhead/system.rb
+++ lib/jarhead/system.rb
@@ -2,7 +2,7 @@ module Jarhead
   module System
 
     def system!( cmd, path = "/dev/null" )
-      `#{cmd} > #{path} 2>&1`
+      system "#{cmd} > #{path} 2>&1"
       return File.open(path, "r").read
     end
 
